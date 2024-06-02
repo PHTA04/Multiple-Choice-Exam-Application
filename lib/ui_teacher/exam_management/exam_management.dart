194c52fea66fa129ad663e0639a3a11cf6eccab5
PHTA04 <phta2612@gmail.com>
2024-06-02 13:57:51 +0700

    Update teacher interface

--- lib/ui_teacher/exam_management/exam_management.dart
+++ lib/ui_teacher/exam_management/exam_management.dart
@@ -15,20 +15,20 @@ class _ExamManagementState extends State<ExamManagement> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child:GridView.count(
-            crossAxisCount: 2,
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
           children: [
-            _examManagementItem("Tạo Đề Thi", Icons.book_outlined, (){
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CreateExam())
-              );
+            _examManagementItem(
+                "Tạo Đề Thi", Icons.book_outlined, Colors.orange, () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const CreateExam()));
             }),
-            _examManagementItem("Xem Đề Thi", Icons.view_list_outlined, (){
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ViewExam())
-              );
+            _examManagementItem(
+                "Xem Đề Thi", Icons.view_list_outlined, Colors.lightGreen, () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const ViewExam()));
             }),
           ],
         ),
@@ -36,26 +36,53 @@ class _ExamManagementState extends State<ExamManagement> {
     );
   }
 
-  _examManagementItem(String title, IconData icon, Null Function() onTap) {
+  _examManagementItem(String title, IconData icon, Color color, VoidCallback onTap) {
     return GestureDetector(
       onTap: onTap,
       child: Card(
         color: Colors.white,
-        elevation: 2,
+        elevation: 5,
         margin: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              icon,
-              size: 50,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              colors: [color.withOpacity(0.6), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            const SizedBox(height: 10),
-            Text(
-              title,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ],
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                size: 50,
+                color: Colors.white,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/ui_teacher/question_bank/question_bank.dart
+++ lib/ui_teacher/question_bank/question_bank.dart
@@ -25,7 +25,6 @@ class _QuestionBankState extends State<QuestionBank> {
     const ExamManagement(),
     const TestAdministration(),
     const ScoreManagement(),
-    // const SignIn(),
   ];
 
   void _onItemTapped(int index) {
@@ -39,68 +38,109 @@ class _QuestionBankState extends State<QuestionBank> {
     return Scaffold(
       appBar: _appBar(),
       body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.grey[200], // Màu nền của bottom navigation bar
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.question_answer),
-            label: 'Ngân Hàng Câu Hỏi',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.admin_panel_settings),
-            label: 'Quản Lý Đề Thi',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.assignment),
-            label: 'Quản Lý Bài Thi',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.score),
-            label: 'Quản Lý Điểm',
-          ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.logout),
-          //   label: 'Đăng Xuất',
-          // ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        onTap: _onItemTapped,
-      ),
+      bottomNavigationBar: _bottomNavigationBar(),
     );
   }
 
   AppBar _appBar() {
     return AppBar(
-      backgroundColor: Colors.white,
       elevation: 10,
+      centerTitle: true,
+      automaticallyImplyLeading: false, // Tắt nút leading (back)
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.purple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+      ),
       title: Text(
-        _appBarTitle(), // Sử dụng phương thức để đặt tiêu đề tương ứng với màn hình
+        _appBarTitle(),
         style: const TextStyle(
           fontSize: 22,
           fontWeight: FontWeight.bold,
-          color: Colors.black,
+          color: Colors.white,
         ),
         textAlign: TextAlign.center,
         overflow: TextOverflow.ellipsis,
       ),
-      leading: GestureDetector(
-        onTap: () {
-          print("tapped");
-        },
-        child: const Icon(
-          Icons.nightlight_round,
-          size: 20,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.logout, color: Colors.white),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const SignIn()),
+            );
+          },
         ),
-      ),
-      actions: const [
-        Icon(
-          Icons.person,
-          size: 20,
-        ),
-        SizedBox(width: 20)
+        const SizedBox(width: 20)
       ],
+    );
+  }
+
+  BottomNavigationBar _bottomNavigationBar() {
+    return BottomNavigationBar(
+      backgroundColor: Colors.white,
+      type: BottomNavigationBarType.fixed,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: _buildIcon(Icons.question_answer_outlined, 0),
+          label: 'Ngân Hàng Câu Hỏi',
+        ),
+        BottomNavigationBarItem(
+          icon: _buildIcon(Icons.admin_panel_settings_outlined, 1),
+          label: 'Quản Lý Đề Thi',
+        ),
+        BottomNavigationBarItem(
+          icon: _buildIcon(Icons.assignment_outlined, 2),
+          label: 'Quản Lý Bài Thi',
+        ),
+        BottomNavigationBarItem(
+          icon: _buildIcon(Icons.score_outlined, 3),
+          label: 'Quản Lý Điểm',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.blueAccent,
+      unselectedItemColor: Colors.grey,
+      onTap: _onItemTapped,
+      showUnselectedLabels: true,
+    );
+  }
+
+  Widget _buildIcon(IconData icon, int index) {
+    bool isSelected = _selectedIndex == index;
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      padding: isSelected ? const EdgeInsets.all(8.0) : const EdgeInsets.all(0),
+      decoration: BoxDecoration(
+        gradient: isSelected
+            ? const LinearGradient(
+          colors: [Colors.blueAccent, Colors.purpleAccent],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        )
+            : null,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: isSelected
+            ? [
+          BoxShadow(
+            color: Colors.blueAccent.withOpacity(0.5),
+            blurRadius: 10,
+            spreadRadius: 1,
+            offset: const Offset(0, 3),
+          ),
+        ]
+            : [],
+      ),
+      child: Icon(
+        icon,
+        size: isSelected ? 25 : 22,
+        color: isSelected ? Colors.white : Colors.grey,
+      ),
     );
   }
 
@@ -114,8 +154,6 @@ class _QuestionBankState extends State<QuestionBank> {
         return "Quản Lý Bài Thi";
       case 3:
         return "Quản Lý Điểm";
-      // case 4:
-      //   return "Đăng Xuất";
       default:
         return "Ngân Hàng Câu Hỏi";
     }
@@ -131,38 +169,40 @@ class QuestionBankPage extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
       child: GridView.count(
         crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
         children: [
-          _questionBankItem("Tạo Môn Học", Icons.book, () {
+          _questionBankItem("Tạo Môn Học", Icons.book_outlined, Colors.purple, () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const CreateSubject()),
             );
           }),
-          _questionBankItem("Xem Môn Học", Icons.view_list, () {
+          _questionBankItem("Xem Môn Học", Icons.view_list_outlined, Colors.teal, () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const ViewSubject()),
             );
           }),
-          _questionBankItem("Tạo Chủ Đề", Icons.category, () {
+          _questionBankItem("Tạo Chủ Đề", Icons.category_outlined, Colors.orange, () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const CreateTopic()),
             );
           }),
-          _questionBankItem("Xem Chủ Đề", Icons.article, () {
+          _questionBankItem("Xem Chủ Đề", Icons.article_outlined, Colors.redAccent, () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const ViewTopic()),
             );
           }),
-          _questionBankItem("Tạo Câu Hỏi", Icons.question_answer, () {
+          _questionBankItem("Tạo Câu Hỏi", Icons.question_answer_outlined, Colors.blue, () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const CreateQuestion()),
             );
           }),
-          _questionBankItem("Xem Câu Hỏi", Icons.view_quilt, () {
+          _questionBankItem("Xem Câu Hỏi", Icons.view_quilt_outlined, Colors.green, () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const ViewQuestion()),
@@ -173,26 +213,53 @@ class QuestionBankPage extends StatelessWidget {
     );
   }
 
-  _questionBankItem(String title, IconData icon, Null Function() onTap) {
+  Widget _questionBankItem(String title, IconData icon, Color color, VoidCallback onTap) {
     return GestureDetector(
       onTap: onTap,
       child: Card(
         color: Colors.white,
-        elevation: 2,
+        elevation: 5,
         margin: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              icon,
-              size: 50,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              colors: [color.withOpacity(0.6), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            const SizedBox(height: 10),
-            Text(
-              title,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ],
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                size: 50,
+                color: Colors.white,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
